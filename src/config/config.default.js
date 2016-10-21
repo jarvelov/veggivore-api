62c944f0bfa061c534c19c46d1ee7b36818b6485
Tobias Järvelöv <tobias@jarvelov.se>
2016-10-21 05:13:01 +0200

    Update default config

--- src/config/config.default.js
+++ src/config/config.default.js
@@ -1,9 +1,14 @@
 const config = {
   name: 'Restify API',
   version: '0.0.1',
-  host: {
-    port: 8080
+  restify: {
+    host: {
+      port: 8080
+    }
+  },
+  mongodb: {
+    database: 'database_name',
+    host: 'localhost'
   }
 };
-
 export default config;
