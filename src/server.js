f76c10071f71d3ebac2de6241d330428518d6900
Tobias Järvelöv <tobias@jarvelov.se>
2016-10-21 05:13:42 +0200

    Configure restify and mongoose

--- src/server.js
+++ src/server.js
@@ -1,13 +1,37 @@
-// Dependencies
+import config from './config/config';
+
+// Restify HTTP server and mongodb
 const restify = require('restify');
-const config = require('./config/config');
 const mongoose = require('mongoose');
 
-// Start server
+// Mongoose config
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://' + config.mongodb.host + '/' + config.mongodb.database);
+
+// Mongoose models
+const models = require('./models')();
+
+// Restify config
 const server = restify.createServer({
-  name: config.name
+  name: config.restify.name,
+  formatters: {
+      'application/json': function(req, res, body, cb) {
+          return cb(null, JSON.stringify(body, null, '\t'));
+      }
+  }
 });
 
-server.listen(config.host.port, function () {
-    console.log('servers listening at %s', server.url);
+const routes = require('./routes')(server, config);
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.bodyParser({
+  mapParams: true,
+  mapFiles: false,
+}));
+server.use(restify.gzipResponse());
+server.use(restify.queryParser());
+
+// Start restify server
+server.listen(config.restify.host.port, () => {
+    console.log('server listening at %s', server.url);
 });
