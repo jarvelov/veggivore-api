d807aa7000a98190973a43a0febad4cfc112eec8
Tobias Järvelöv <tobias@jarvelov.se>
2016-11-16 22:20:36 +0100

    Move general routes to their own file

--- src/routes/pages/index.js
+++ src/routes/pages/index.js
@@ -3,89 +3,10 @@ const passport = require('passport-restify');
 
 module.exports = (server, models, config) => {
 
-  // Delete a page
+  // Require the general page routes
+  require('./general')(server, models, config);
 
-  server.del({
-    path: '/pages/:uri',
-    validation: {
-      id: {
-        isRequired: true
-      }
-    }
-  }, passport.authenticate('jwt'), (req, res, next) => {
-    return models.Pages.remove({
-        uri: req.params.uri
-      })
-      .then(result => {
-        res.json({
-          success: true
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        return next(err);
-      });
-  });
-
-  // Get a page
-
-  server.get({
-    path: '/pages/:uri',
-    validation: {
-      uri: {
-        isRequired: true
-      }
-    }
-  }, (req, res, next) => { //No authentication required
-    return models.Pages.findOne({
-        uri: req.params.uri
-      })
-      .then(result => {
-        if (!result) {
-          throw new restify.NotFoundError('Page with URI ' + req.params.uri + ' was not found');
-        }
-        return result;
-      })
-      .then(page => {
-        res.json({
-          success: true,
-          data: page
-        });
-      })
-      .catch(err => {
-        return next(err);
-      });
-  });
-
-  // Update a page
-
-  server.put({
-    path: '/pages/:uri',
-    validation: {
-      resources: {
-        uri: {
-          isRequired: true
-        }
-      }
-    }
-  }, passport.authenticate('jwt'), (req, res, next) => {
-    return models.Pages.update({
-        params: {}
-      }, {
-        uri: req.params.uri
-      })
-      .then(result => {
-        res.json({
-          success: true
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        return next(err);
-      });
-  });
-
-  // Require the specific pages
+  // Require the specific page routes
 
   require('./cafes')(server, models, config);
   require('./facts')(server, models, config);
