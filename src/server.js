d2eca15b09155a7ae120cb966f67994a88f8c26b
Tobias Järvelöv <tobias@jarvelov.se>
2016-11-17 12:10:28 +0100

    Handle both dev and prod env

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Load configuration
-import config from './config/config';
+const config = require('./config/config');
 
 // Mongoose
 const mongoose = require('mongoose');
@@ -15,11 +15,36 @@ const models = require('./models')(config);
 
 // Restify server
 const server = require('./config/restify')(models, config);
-
 // Set up the routes for restify server
-const routes = require('./routes')(server, models, config);
+require('./routes')(server, models, config);
 
-// Start restify server
-server.listen(config.restify.host.port, () => {
-    console.log('server listening at %s', server.url);
+// Only fire up a server with webpack in watch mode when in development
+if (process.env.NODE_ENV !== 'production') {
+  const Webpack = require('webpack');
+  const webpackConfig = require('../webpack.dev.config.js');
+
+  // First we fire up Webpack and pass in the configuration
+  const compiler = Webpack(webpackConfig);
+
+  const devMiddleware = require('webpack-dev-middleware')(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    stats: {
+      colors: true,
+      chunks: false
+    }
+  });
+
+  // serve webpack bundle output
+  server.use(devMiddleware);
+}
+
+// Start server
+module.exports = server.listen(config.restify.host.port, function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  let uri = 'http://localhost:' + config.restify.host.port;
+  console.log('Listening at ' + uri + '\n');
 });
