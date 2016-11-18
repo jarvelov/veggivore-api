5a6f71afcf82c26b7c16bbe3926d02dac3e3f1ac
Tobias Järvelöv <tobias@jarvelov.se>
2016-11-18 01:58:40 +0100

    Add uri and label, set required attributes

--- src/models/embedded/sources.js
+++ src/models/embedded/sources.js
@@ -4,16 +4,27 @@ const autopopulate = require('mongoose-autopopulate');
 
 module.exports = (models, config) => {
   const Sources = new Schema({
+    uri: {
+      required: true,
+      type: String
+    },
+    label: {
+      required: true,
+      type: String
+    },
     user: {
+      required: true,
       autopopulate: true,
       type: Schema.Types.ObjectId,
       ref: 'Users'
     },
     page: {
+      required: true,
       type: Schema.Types.ObjectId,
       ref: 'Pages'
     },
     revision: {
+      required: true,
       type: Schema.Types.ObjectId,
       ref: 'Revisions'
     }
