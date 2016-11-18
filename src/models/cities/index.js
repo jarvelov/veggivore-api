44fd66448a2cc22664ea3182b485f86e18641534
Tobias Järvelöv <tobias@jarvelov.se>
2016-11-19 00:59:47 +0100

    Add unique slug attr, add geocode location attr

--- src/models/cities/index.js
+++ src/models/cities/index.js
@@ -4,13 +4,24 @@ const Schema = mongoose.Schema;
 module.exports = (models, config) => {
   const Cities = new Schema({
     name: {
+      required: true,
       type: String
+    },
+    slug: {
+      required: true,
+      unique: true,
+      index: true,
+      type: String
+    },
+    location: {
+      type: [models.Embedded.Coordinates.schema]
     }
   }, {
     timestamps: true
   });
 
   Cities.set('toJSON', {
+    versionKey: false,
     virtuals: true
   });
 
