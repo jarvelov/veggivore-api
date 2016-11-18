042a02c2b06a603e9d16a2eef1e5fd7a758b8eb2
Tobias Järvelöv <tobias@jarvelov.se>
2016-11-18 01:55:47 +0100

    Started working on a route to POST and GET fact pages, feels pretty decent imo

--- src/routes/facts/index.js
+++ src/routes/facts/index.js
@@ -1,4 +1,6 @@
 const passport = require('passport-restify');
+const slug = require('slug');
+const _ = require('lodash');
 
 module.exports = (server, models, config) => {
   server.post({
@@ -12,14 +14,16 @@ module.exports = (server, models, config) => {
           'isRequired': true
         },
         'sources': {
-          'isRequired': false,
+          'isRequired': true,
           'isArray': true // TODO: This validator doesn't exist, create it
         },
         'categories': {
-          'isRequired': true
+          'isRequired': true,
+          'isArray': true // TODO: This validator doesn't exist, create it
         },
         'tags': {
-          'isRequired': false
+          'isRequired': false,
+          'isArray': true // TODO: This validator doesn't exist, create it
         },
         'images': {
           'isRequired': false,
@@ -31,17 +35,129 @@ module.exports = (server, models, config) => {
       }
     }
   }, passport.authenticate('jwt'), (req, res, next) => {
-    const Page = new models.Pages(req.params);
-    return Page.save()
-      .then(result => {
-        res.json({
-          success: true,
-          data: result
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        return next(err);
+    return new Promise((resolve, reject) => {
+      const props = _.extend({}, req.params);
+      props.uri = slug(props.title, {
+        lower: true
       });
+
+      // Array to hold all the database operations that need to be performed (promises)
+      const operations = [];
+
+      // First create the Revision object
+      const Revision = new models.Revisions.Facts({
+        title: props.title,
+        content: props.content,
+        user: req.user.id
+      });
+      operations.push(Revision);
+
+      // Then the general Page object
+      const Page = new models.Pages({
+        uri: props.uri,
+        user: req.user.id,
+        revision: {
+          current: Revision.id
+        }
+      });
+      operations.push(Page);
+
+      // Now configure a Contributors object
+      const Contributors = new models.Contributors({
+        user: req.user.id,
+        page: Page.id,
+        revision: Revision.id
+      });
+      operations.push(Contributors);
+
+      // Then set the Contributors object to the Page object so they both refer to each other
+      Page.contributors = [Contributors.id];
+
+      // Then set up the sources, if there are any
+      if (props.sources && props.sources.length > 0) {
+        try {
+          Revision.sources = props.sources.map(item => {
+            return new models.Embedded.Sources({
+              uri: item.uri,
+              label: item.label,
+              user: req.user.id,
+              page: Page.id,
+              revision: Revision.id
+            });
+          });
+        } catch (err) {
+          throw new Error(err);
+        }
+      }
+
+      resolve(operations);
+    })
+    .then(operations => {
+      operations = operations.map(o => (o.save()));
+      console.log('hello');
+      return Promise
+        .all(operations)
+        .then(result => {
+          // Okay, so we've saved all the things we need, do we need to do anything else here?
+          console.log(result);
+        });
+    })
+    .then(result => {
+      res.json({
+        success: true,
+        data: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      return next(err);
+    });
+  });
+
+  server.get({
+    'path': '/facts',
+    'validation': {
+      'queries': {
+        'title': {
+          'isRequired': false
+        },
+        'categories': {
+          'isRequired': false,
+          'isArray': true // TODO: This validator doesn't exist, create it
+        },
+        'tags': {
+          'isRequired': false,
+          'isArray': true // TODO: This validator doesn't exist, create it
+        }
+      }
+    }
+  }, passport.authenticate('jwt'), (req, res, next) => {
+    const props = _.extend({}, req.params);
+    const query = {};
+
+    const filters = Object.keys(props).map(key => {
+      let obj = {};
+      if (props.hasOwnProperty(key) && typeof props[key] !== 'undefined' && props[key] !== null) {
+        obj[key] = props[key];
+      }
+      return obj;
+    })
+    .filter(query => (query));
+
+    if (filters.length > 0) {
+      query.$and = filters;
+    }
+
+    return models.Pages.findOne(query)
+    .then(result => {
+      res.json({
+        success: true,
+        data: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      return next(err);
+    });
   });
 };
