de9ccb8e7039aff0e4ec07c773d0335a3294e631
Tobias Järvelöv <tobias@jarvelov.se>
2016-11-19 01:06:56 +0100

    Load coordinates model

--- src/models/embedded/index.js
+++ src/models/embedded/index.js
@@ -1,6 +1,7 @@
 module.exports = (models, config) => {
   models.Embedded = {};
   models.Embedded.ContactDetails = require('./contact-details')(models, config);
+  models.Embedded.Coordinates = require('./coordinates')(models, config);
   models.Embedded.Locations = require('./locations')(models, config);
   models.Embedded.Sources = require('./sources')(models, config);
 
