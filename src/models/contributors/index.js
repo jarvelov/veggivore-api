235ffd88f973d952463502680a36b990c9377a18
Tobias Järvelöv <tobias@jarvelov.se>
2016-11-19 01:06:11 +0100

    Set required property, disable _v key

--- src/models/contributors/index.js
+++ src/models/contributors/index.js
@@ -5,17 +5,25 @@ const autopopulate = require('mongoose-autopopulate');
 module.exports = (models, config) => {
   const Contributors = new Schema({
     user: {
+      required: true,
       autopopulate: true,
       type: Schema.Types.ObjectId,
       ref: 'Users'
     },
     page: {
+      required: true,
       type: Schema.Types.ObjectId,
       ref: 'Pages'
     },
     revision: {
+      required: true,
       type: Schema.Types.ObjectId,
       ref: 'Revisions'
+    },
+    anonymous: {
+      required: true,
+      type: Boolean,
+      default: false
     }
   }, {
     timestamps: true
@@ -24,6 +32,7 @@ module.exports = (models, config) => {
   Contributors.plugin(autopopulate);
 
   Contributors.set('toJSON', {
+    versionKey: false,
     virtuals: true
   });
 
