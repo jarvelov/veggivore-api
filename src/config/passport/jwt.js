1fa5021a7ba62b548f00e3c697c2dfb5733c84d0
Tobias Järvelöv <tobias@jarvelov.se>
2016-11-19 18:18:00 +0100

    Fix indentation

--- src/config/passport/jwt.js
+++ src/config/passport/jwt.js
@@ -1,25 +1,24 @@
-const JwtStrategy = require('passport-jwt')
-  .Strategy;
-const ExtractJwt = require('passport-jwt')
-  .ExtractJwt;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 module.exports = (server, models, config) => {
-  const opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
-  opts.secretOrKey = config.restify.authentication.secret;
+  const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeader(),
+    secretOrKey: config.restify.authentication.secret
+  };
 
   return new JwtStrategy(opts, (payload, cb) => {
     return models.Users
-      .findOne({
-        _id: payload.userId
-      })
-      .then(user => {
-        if (user) {
-          cb(null, user);
-        } else {
-          let err = new Error('User not found');
-          cb(err);
-        }
-      });
+    .findOne({
+      _id: payload.userId
+    })
+    .then(user => {
+      if (user) {
+        cb(null, user);
+      } else {
+        let err = new Error('User not found');
+        cb(err);
+      }
+    });
   });
 };
--- src/config/passport/local.js
+++ src/config/passport/local.js
@@ -1,5 +1,4 @@
-const LocalStrategy = require('passport-local')
-  .Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const jwt = require('jsonwebtoken');
 
 module.exports = (server, models, config) => {
@@ -9,25 +8,25 @@ module.exports = (server, models, config) => {
     session: false
   }, (email, password, cb) => {
     return models.Users
-      .findOne({
-        email: email
-      })
-      .select('+password +email')
-      .then(user => {
-        return user.comparePassword(password, user.password)
-          .then(isMatch => {
-            if (isMatch) {
-              cb(null, {
-                user: user.id,
-                token: jwt.sign({
-                  userId: user.id
-                }, config.restify.authentication.secret)
-              });
-            } else {
-              let err = new Error('Password did not match');
-              cb(err);
-            }
+    .findOne({
+      email: email
+    })
+    .select('+password +email')
+    .then(user => {
+      return user.comparePassword(password, user.password)
+      .then(isMatch => {
+        if (isMatch) {
+          cb(null, {
+            user: user.id,
+            token: jwt.sign({
+              userId: user.id
+            }, config.restify.authentication.secret)
           });
+        } else {
+          let err = new Error('Password did not match');
+          cb(err);
+        }
       });
+    });
   });
 };
