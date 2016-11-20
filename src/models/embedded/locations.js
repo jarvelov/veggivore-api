c825c1dff39bbfecb6bb11b24275f8ce22a2e9b6
Tobias Järvelöv <tobias@jarvelov.se>
2016-11-20 05:47:22 +0100

    Include part of Coordinates and ContactDetails schemas

--- src/models/embedded/locations.js
+++ src/models/embedded/locations.js
@@ -4,19 +4,51 @@ const Schema = mongoose.Schema;
 module.exports = (models, config) => {
   const Locations = new Schema({
     city: {
+      required: true,
       autopopulate: true,
       type: Schema.Types.ObjectId,
       ref: 'Cities'
     },
     address: {
+      required: true,
       type: String
+    },
+    geocode: {
+      required: true,
+      type: [Number],
+      index: '2dsphere'
     },
     zipcode: {
       type: Number
     },
-    location: {
-      type: [models.Embedded.Coordinates]
+    openhours: {
+      monday: {
+        type: [String]
+      },
+      tuesday: {
+        type: [String]
+      },
+      wednesday: {
+        type: [String]
+      },
+      thursday: {
+        type: [String]
+      },
+      friday: {
+        type: [String]
+      },
+      saturday: {
+        type: [String]
+      },
+      sunday: {
+        type: [String]
+      }
     }
+  });
+
+  Locations.set('toJSON', {
+    versionKey: false,
+    virtuals: true
   });
 
   return Locations;
