a9f4a1e12a063e0c56531467b1339ebc99610130
Tobias Järvelöv <tobias@jarvelov.se>
2016-11-20 05:53:20 +0100

    Load stores

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,9 +14,10 @@ module.exports = (server, models, config) => {
 
   // Require the specific page routes
   require('./cafes')(server, models, config);
+  require('./companies')(server, models, config);
   require('./facts')(server, models, config);
   require('./products')(server, models, config);
   require('./recipes')(server, models, config);
   require('./restaurants')(server, models, config);
-  require('./companies')(server, models, config);
+  require('./stores')(server, models, config);
 };
