e94c717b87168e140191235460a7c552a89f6c13
Tobias Järvelöv <tobias@jarvelov.se>
2016-11-20 05:53:03 +0100

    Remove GET and DELETE endpoints, the general /pages routes should handle that

--- src/routes/facts/index.js
+++ src/routes/facts/index.js
@@ -1,15 +1,64 @@
 /**
- * @name Routes/Facts
+ * @namespace Routes/Facts
  * @file /routes/facts/index.js
- * @module routes/facts
+ * @module /routes/facts
  */
 
 const restify = require('restify');
 const passport = require('passport-restify');
 const slug = require('slug');
 const _ = require('lodash');
+const url = require('url');
 
 module.exports = (server, models, config) => {
+  /**
+   * Create fact page
+   * @function
+   *
+   * @name create
+   * @version 1
+   * @method POST
+   * @param {String} title The title of the page
+   * @param {String} content The page's content
+   * @param {ObjectId[]} categories ObjectId Array of {@link Models#Categories}
+   * @param {String[]} [sources] Array of source links (urls) used in the page
+   * @param {ObjectId[]} [products] ObjectId Array of {@link Models#Products}
+   * @param {Object[]} [tags] ObjectId Array of {@link Models#Tags}
+   * @param {ObjectId[]} [images] ObjectId Array of {@link Models#Images}
+   * @param {Boolean} [anonymous=false] Whether the user should be shown as the author or among the contributors
+   * @example
+   * <caption>Create a fact page</caption>
+   * <pre>
+   * POST /facts
+   * {
+   *   title: {
+   *     "D-vitamin",
+   *   },
+   *   content: {
+   *     "Livsmedelsverket uppmanar alla i Sverige att äta D-vitamintillskott. Bioaktiviteten är något lägre för vitamin D2 än för vitamin D3.",
+   *   },
+   *   sources: ["http://www.livsmedelsverket.se/livsmedel-och-innehall/naringsamne/vitaminer-och-antioxidanter/vitamin-d/"],
+   *   openhours: {
+   *     monday: ["08:00","14:00","16:00','22:00']
+   *   }
+   * }
+   * </pre>
+   * @returns {JSON}
+   * <caption>On success</caption>
+   * <pre>
+   * {
+   *   "success": true,
+   *   "data": "5830dce08470d31bee32d177" // The new page's ObjectId
+   * }
+   * </pre>
+   * <caption>On failure</caption>
+   * <pre>
+   * {
+   *   "success": false,
+   *   "error": [...]
+   * }
+   * </pre>
+   */
   server.post({
     path: '/facts',
     validation: {
@@ -20,7 +69,7 @@ module.exports = (server, models, config) => {
         content: {
           isRequired: true
         },
-        sources: {
+        sources: { // TODO: Write a isStringArray validator
           isRequired: true,
           isArray: true
         },
@@ -52,12 +101,14 @@ module.exports = (server, models, config) => {
 
     return new Promise((resolve, reject) => {
       // First set up the general page object first
-      let page = new models.Pages({
+      operations.page = new models.Pages({
         url: params.url,
-        user: params.user.id
+        user: params.user.id,
+        contributors: [{
+          user: params.user.id,
+          anonymous: params.anonymous
+        }]
       });
-
-      operations.page = page;
 
       resolve();
     })
@@ -78,33 +129,21 @@ module.exports = (server, models, config) => {
         history: [revision.id]
       };
       // Lastly set up the sources
-      revision.sources = params.sources.map(item => {
+      revision.sources = params.sources.map(source => {
+        let hostname = url.parse(source).hostname;
         return {
-          url: item.url,
-          label: item.label
+          url: source,
+          label: hostname
         };
       });
 
       operations.revision = revision;
     })
     .then(() => {
-      // Now configure a Contributors object
-      let contributor = new models.Contributors({
-        user: params.user.id,
-        page: operations.page.id,
-        revision: operations.revision.id,
-        anonymous: params.anonymous
-      });
-
-      operations.contributor = contributor;
-
-      // Then set the Contributors object to the page object so they both refer to each other
-      operations.page.contributors = [contributor.id];
-    })
-    .then(() => {
       let inserts = Object.keys(operations).map(k => (operations[k].save()));
       return Promise.all(inserts)
       .catch(err => {
+        console.log(err);
         if (err.code === 11000) {
           throw new restify.errors.BadRequestError('Title must be unique');
         } else {
@@ -115,10 +154,11 @@ module.exports = (server, models, config) => {
     .then(result => {
       res.json({
         success: true,
-        data: result
+        data: operations.page.id
       });
     })
     .catch(err => {
+      console.log(err);
       return next(err);
     });
   });
@@ -129,16 +169,15 @@ module.exports = (server, models, config) => {
    *
    * @name update
    * @memberof: Routes/Facts/PUT
+   * @method: PUT
    * @version 1:
-   * @param {String} title The title of the page
-   * @param {String} content The page's content
-   * @param {Object[]} sources Source links used in the page
-   * @param {String} sources[].url - The url of the source link
-   * @param {String} sources[].label - The label of the source
-   * @param {String[]} categories Array of {@link Models#Categories} ID's
-   * @param {String[]} tags Array of {@link Models#Tags} ID's
-   * @param {String[]} images Array of {@link Models#Images} ID's
-   * @param {Boolean} anonymous Whether the user should be visible
+   * @param {String} [title] The title of the page
+   * @param {String} [content] The page's content
+   * @param {String[]} [sources] Array of source links (urls) used in the page
+   * @param {String[]} [categories] Array of {@link Models#Categories} ID's
+   * @param {String[]} [tags] Array of {@link Models#Tags} ID's
+   * @param {String[]} [images] Array of {@link Models#Images} ID's
+   * @param {Boolean} [anonymous=false] Whether the user should be shown among contributors
    * @returns {JSON}
    */
   server.put({
@@ -157,7 +196,9 @@ module.exports = (server, models, config) => {
         },
         sources: {
           isRequired: true,
-          isArray: true // TODO: Write a validator for sources
+          isObjectArray: {
+            keys: ['url', 'label']
+          }
         },
         categories: {
           isRequired: true,
@@ -190,11 +231,24 @@ module.exports = (server, models, config) => {
     })
     .then(page => {
       // Check if query returned our page object
-      if (page) {
-        operations.page = page;
-      } else {
+      if (!page) {
         // Else notify the user that we didn't find what we were looking for
         throw new restify.errors.NotFoundError('Page with ID ' + req.params.id + ' was not found');
+      } else {
+        operations.page = page;
+
+        // Check if user has contributed to this page before
+        let contributed = operations.page.contributors.reduce((contributed, contributor) => {
+          return contributed || contributor.id === params.user.id;
+        }, false);
+
+        // If not add user to contributors
+        if (!contributed) {
+          operations.page.contributors.push({
+            user: params.user.id,
+            anonymous: params.anonymous
+          });
+        }
       }
     })
     .then(() => {
@@ -205,57 +259,22 @@ module.exports = (server, models, config) => {
         page: operations.page.id,
         user: params.user.id,
         categories: params.categories,
-        tags: params.tags
-      });
-
-      // Then set up the revision's sources
-      revision.sources = params.sources.map((item, i) => {
-        let source = {
-          url: item.url,
-          label: item.label
-        };
-
-        return source;
+        tags: params.tags,
+        sources: params.sources.map(source => {
+          let hostname = url.parse(source).hostname;
+          return {
+            url: source,
+            label: hostname
+          };
+        })
       });
 
       // Set the new revision object to be the page's current one
       operations.page.revision.current = revision.id;
       // Push it to the history as well
       operations.page.revision.history.push(revision.id);
-
       // Add the revision's object to be inserted
       operations.revision = revision;
-    })
-    .then(() => {
-      // Now check if user has contributed to this page before, else create a Contributors object
-      return models.Contributors.findOne({
-        user: params.user.id,
-        page: operations.page.id
-      })
-      .then(contributor => {
-        if (contributor) {
-          return contributor; // User's contributors object already exists
-        } else {
-          // Create a new contributors object for this user and page
-          let contributor = new models.Contributors({
-            user: params.user.id,
-            page: operations.page.id,
-            revision: operations.revision.id,
-            anonymous: params.anonymous
-          });
-
-          // Add the Contributor object to be inserted
-          operations.contributor = contributor;
-
-          return contributor;
-        }
-      })
-      .then(contributor => {
-        // Then add the Contributors object ID to the page object's contributors array, if it's not already there
-        if (operations.page.contributors.indexOf(contributor.id) === -1) {
-          operations.page.contributors.push(contributor.id);
-        }
-      });
     })
     .then(() => {
       // Save all operations to insert them to the database
@@ -265,56 +284,6 @@ module.exports = (server, models, config) => {
     .then(result => {
       res.json({
         success: true
-      });
-    })
-    .catch(err => {
-      return next(err);
-    });
-  });
-
-  server.get({
-    path: '/facts',
-    validation: {
-      queries: {
-        title: {
-          isRequired: false
-        },
-        url: {
-          isRequired: false,
-          isUrl: true
-        },
-        categories: {
-          isRequired: false,
-          isObjectIdArray: true
-        },
-        tags: {
-          isRequired: false,
-          isObjectIdArray: true
-        }
-      }
-    }
-  }, passport.authenticate('jwt'), (req, res, next) => {
-    const params = _.merge({}, req.params);
-    const query = {};
-
-    const filters = Object.keys(params).map(key => {
-      let obj = {};
-      if (params.hasOwnProperty(key) && typeof params[key] !== 'undefined' && params[key] !== null) {
-        obj[key] = params[key];
-      }
-      return obj;
-    })
-    .filter(query => (query));
-
-    if (filters.length > 0) {
-      query.$and = filters;
-    }
-
-    return models.Pages.find(query)
-    .then(result => {
-      res.json({
-        success: true,
-        data: result
       });
     })
     .catch(err => {
