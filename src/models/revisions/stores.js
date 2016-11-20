653578a09923c68da0bfcf173ae926abd532a655
Tobias Järvelöv <tobias@jarvelov.se>
2016-11-20 05:50:41 +0100

    Add static method to get an extended revision object for Stores by passing in params

--- src/models/revisions/stores.js
+++ src/models/revisions/stores.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const autopopulate = require('mongoose-autopopulate');
 require('mongoose-schema-extend'); // Just requiring it sets up the schema extension plugin
+const _ = require('lodash');
 
 module.exports = (models, config) => {
   const RevisionsStore = models.Revisions.Default.schema.extend({
@@ -35,6 +36,26 @@ module.exports = (models, config) => {
     collection: 'revisions',
     timestamps: true
   });
+
+  RevisionsStore.statics.createRevision = (params) => {
+    // Grab a copy of a default revision object
+    let defaultRevision = models.Revisions.Default.createRevision(params);
+    // Add or modify the attributes for this revision type
+    return _.extend(defaultRevision, {
+      company: params.company,
+      contact: {
+        phones: params.phones,
+        websites: params.websites
+      },
+      location: {
+        city: params.city,
+        openhours: params.openhours,
+        coordinates: params.coordinates,
+        address: params.address,
+        zipcode: params.zipcode
+      }
+    });
+  };
 
   RevisionsStore.plugin(autopopulate);
 
